366d80c0f2ebf4bb5fff79b7d6017aa1b98b24dc
John 9e9 <jn9e9e9@gmail.com>
2021-03-18 00:04:23 +0000

    fix shellcheck
    Signed-off-by: John 9e9 <jn9e9e9@gmail.com>

--- e2etest/scripts/ci-pkcs11.sh
+++ e2etest/scripts/ci-pkcs11.sh
@@ -7,11 +7,11 @@
 # When complete will run tests only on pkcs11 provider
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TESTDIR=$(realpath ${SCRIPTDIR}/..)
+TESTDIR=$(realpath "${SCRIPTDIR}"/..)
 
 set -eouf pipefail 
 
-pushd ${TESTDIR}
+pushd "${TESTDIR}"
 docker build -t pkcs11-provider "${TESTDIR}"/provider_cfg/pksc11
 docker run -v "$(realpath "${TESTDIR}"/..)":/tmp/parsecgo -w /tmp/parsecgo pkcs11-provider /tmp/parsecgo/e2etest/scripts/ci.sh pkcs11
 popd
--- e2etest/scripts/ci-tpm.sh
+++ e2etest/scripts/ci-tpm.sh
@@ -7,11 +7,11 @@
 # When complete will run tests only on tpm provider
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TESTDIR=$(realpath ${SCRIPTDIR}/..)
+TESTDIR=$(realpath "${SCRIPTDIR}"/..)
 
 set -eouf pipefail 
 
-pushd ${TESTDIR}
+pushd "${TESTDIR}"
 docker build -t tpm-provider "${TESTDIR}"/provider_cfg/tpm
 docker run -v "$(realpath "${TESTDIR}"/..)":/tmp/parsecgo -w /tmp/parsecgo tpm-provider /tmp/parsecgo/e2etest/scripts/ci.sh tpm
 popd
